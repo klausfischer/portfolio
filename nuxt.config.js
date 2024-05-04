f57d47793a0d0fcd0dc4773d3c0c62fb6f00e2e1
Klaus Fischer <klausfisc@gmail.com>
2024-05-04 19:53:02 +0200

    chore: init

---
+++ nuxt.config.js
@@ -0,0 +1,15 @@
+export default defineNuxtConfig({
+  css: ['@/assets/css/roboto.css'],
+  modules: [
+    [
+      '@storyblok/nuxt',
+      {
+        accessToken: 'UY0Zk98v0VDsl3T43ymJVAtt',
+        apiOptions: {
+          region: '' // Set 'US" if your space is created in US region (EU default)
+        }
+      },
+    ],
+    '@nuxtjs/tailwindcss',
+  ]
+})
